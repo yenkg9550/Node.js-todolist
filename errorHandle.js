65e3c81ef7673edae786d625fea6d84aca48ff4e
eric <ana31005@gmail.com>
2023-01-20 03:40:42 +0800

    first commit

---
+++ errorHandle.js
@@ -0,0 +1,17 @@
+const headers = {
+  "Access-Control-Allow-Headers": 'Conent-Type, Authorization, Content-Length, X-Requested-With',
+  "Access-Control-Allow-Origin": '*',
+  "Access-Control-Allow-Methods": 'PATCH, POST, GET, OPTIONS, DELETE',
+  "Conent-Type": 'application/json'
+}
+
+function errorHandle(res) {
+  res.writeHead(404, headers); 
+  res.write(JSON.stringify({
+    "status": "false",
+    "message": '欄位未填寫正確，或無此 todo id' ,
+  }));
+  res.end()
+}
+
+module.exports = errorHandle
